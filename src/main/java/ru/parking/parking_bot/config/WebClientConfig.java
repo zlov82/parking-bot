b9d09541c1b9e82f107e638ca9d2fd7431ec87e1
akozlov <andy.kozlov@gmail.com>
2026-04-12 00:05:15 +0300

    fix more than one number upload

--- src/main/java/ru/parking/parking_bot/config/WebClientConfig.java
+++ src/main/java/ru/parking/parking_bot/config/WebClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.HttpProtocol;
 import reactor.netty.http.client.HttpClient;
@@ -41,9 +42,13 @@ public class WebClientConfig {
     @Bean
     @Qualifier("telegramClient")
     public WebClient telegramWebClient(ProxyProperties proxy) {
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(config -> config.defaultCodecs().maxInMemorySize(1024 * 1024))
+                .build();
         return WebClient.builder()
                 .baseUrl("https://api.telegram.org")
                 .clientConnector(buildConnector(proxy))
+                .exchangeStrategies(strategies)
                 .build();
     }
 
